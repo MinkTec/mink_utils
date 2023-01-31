92ba8a76c0678fb35e68f2fa19712978dea819ee
Arwed Masch <arwed.masch@gmail.com>
2023-01-31 11:49:23 +0100

    Return bool in TimerFutureWrapper (WIP)

--- lib/future_utils.dart
+++ lib/future_utils.dart
@@ -2,25 +2,29 @@ import 'dart:async';
 
 /// A Future Wrapper that completes in a minimum amount of time
 class TimerFutureWrapper<T> {
-  Future<T> future;
-  late Future<T> completerFuture;
+  Future<bool> future;
+  late Future<bool> completerFuture;
   Duration minDuration;
   bool taskCompleted = false;
   bool timerCompleted = false;
+  bool success = false;
 
   TimerFutureWrapper({required this.future, required this.minDuration}) {
     completerFuture = timedFuture();
   }
 
-  Future<T> timedFuture() async {
-    final completer = Completer<T>();
+  Future<bool> timedFuture() async {
+    final completer = Completer<bool>();
     Timer(minDuration, () async {
-      if (taskCompleted) completer.complete();
+      if (taskCompleted) completer.complete(success);
       timerCompleted = true;
     });
     future.then((value) {
       taskCompleted = true;
-      if (timerCompleted) completer.complete();
+      success = value;
+      if (timerCompleted) {
+        completer.complete(success);
+      }
     });
     return completer.future;
   }
