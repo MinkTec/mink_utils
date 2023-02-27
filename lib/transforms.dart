0c3233c6dd05800a459ececb708b61ddc43a9d3a
Jonas Walkling <jonas@walkling.email>
2023-02-27 14:37:16 +0100

    Switch to non static class for list coordinate transforms

--- lib/transforms.dart
+++ lib/transforms.dart
@@ -1,66 +1,93 @@
 import 'dart:math' as math;
 import 'dart:typed_data';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/painting.dart';
+import 'package:mink_utils/list_utils.dart';
 
-class RawPointTransformer {
-  static void rotate(Float32List b, double angle) {
+class RawCurve {
+  Float32List points;
+
+  RawCurve(this.points);
+
+  factory RawCurve.fromList(List<double> list) =>
+      RawCurve(Float32List.fromList(list));
+
+  factory RawCurve.copy(Float32List l) => RawCurve.fromList(l.copy());
+
+  RawCurve copy() => RawCurve.copy(points);
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) {
+      return true;
+    } else if (other is RawCurve) {
+      return listEquals(points, other.points);
+    } else if (other is List<double> || other is Float32List) {
+      return listEquals(points, other as List<double>);
+    } else {
+      return false;
+    }
+  }
+
+  void rotate(double angle) {
     double temp;
-    for (int i = 0; i < b.length; i += 2) {
-      temp = b[i] * math.cos(angle) - b[i + 1] * math.sin(angle);
-      b[i + 1] = b[i] * math.sin(angle) + b[i + 1] * math.cos(angle);
-      b[i] = temp;
+    for (int i = 0; i < points.length; i += 2) {
+      temp = points[i] * math.cos(angle) - points[i + 1] * math.sin(angle);
+      points[i + 1] =
+          points[i] * math.sin(angle) + points[i + 1] * math.cos(angle);
+      points[i] = temp;
     }
   }
 
-  static void translate(Float32List b, double dx, double dy) {
-    for (int i = 0; i < b.length; i += 2) {
-      b[i] = b[i] + dx;
-      b[i + 1] = b[i + 1] + dy;
+  void translate(double dx, double dy) {
+    for (int i = 0; i < points.length; i += 2) {
+      points[i] = points[i] + dx;
+      points[i + 1] = points[i + 1] + dy;
     }
   }
 
-  static void scale(Float32List b, double scale) {
-    assert(b.length > 3);
+  void scale(double scale) {
+    assert(points.length > 3);
 
-    final baseX = b[0];
-    final baseY = b[1];
+    final baseX = points[0];
+    final baseY = points[1];
 
-    for (int i = 2; i < b.length; i += 2) {
-      b[i] = (b[i] - baseX) * scale;
-      b[i + 1] = (b[i + 1] - baseY) * scale;
+    for (int i = 2; i < points.length; i += 2) {
+      points[i] = (points[i] - baseX) * scale;
+      points[i + 1] = (points[i + 1] - baseY) * scale;
     }
   }
 
-  static void scaleX(Float32List b, double scale) {
-    assert(b.length > 3);
-    final baseX = b[0];
-    for (int i = 2; i < b.length; i += 2) {
-      b[i] = (b[i] - baseX) * scale;
+  void scaleX(double scale) {
+    assert(points.length > 3);
+    final baseX = points[0];
+    for (int i = 2; i < points.length; i += 2) {
+      points[i] = (points[i] - baseX) * scale;
     }
   }
 
-  static void scaleY(Float32List b, double scale) {
-    assert(b.length > 3);
-    final baseY = b[1];
-    for (int i = 2; i < b.length; i += 2) {
-      b[i + 1] = (b[i + 1] - baseY) * scale;
+  void scaleY(double scale) {
+    assert(points.length > 3);
+    final baseY = points[1];
+    for (int i = 2; i < points.length; i += 2) {
+      points[i + 1] = (points[i + 1] - baseY) * scale;
     }
   }
 
-  /// returns the bounding box as rectangle [top, left, width, height]
-  static Rect getBoundingBox(Float32List b) {
-    final List<double> rect = [b[0], b[1], b[0], b[1]];
+  Rect get boundingBox {
+    final List<double> rect = [points[0], points[1], points[0], points[1]];
 
-    for (int i = 2; i < b.length; i += 2) {
-      if (rect[0] > b[i]) rect[0] = b[i];
-      if (rect[1] > b[i + 1]) rect[1] = b[i + 1];
-      if (rect[2] < b[i]) rect[2] = b[i];
-      if (rect[3] < b[i + 1]) rect[3] = b[i + 1];
+    for (int i = 2; i < points.length; i += 2) {
+      if (rect[0] > points[i]) rect[0] = points[i];
+      if (rect[1] > points[i + 1]) rect[1] = points[i + 1];
+      if (rect[2] < points[i]) rect[2] = points[i];
+      if (rect[3] < points[i + 1]) rect[3] = points[i + 1];
     }
+    assert(rect[0] <= rect[2] && rect[1] <= rect[3]);
 
-    return Rect.fromLTRB(b[0], b[1], b[2], b[3]);
+    return Rect.fromLTRB(rect[0], rect[1], rect[2], rect[3]);
   }
 
-  static Offset getCenter(Float32List b) => getBoundingBox(b).center;
+  Offset get center => boundingBox.center;
 }
