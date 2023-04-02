2ea45e7128122972cc5bdda9da46273e8f0da585
Jonas Walkling <jonas@walkling.email>
2023-04-02 15:28:03 +0200

    Add finite getter for num and double to prevent nan or inf errors

--- lib/basic_utils.dart
+++ lib/basic_utils.dart
@@ -13,3 +13,13 @@ Future<void> sleepms(int ms) {
   Timer(Duration(milliseconds: ms), () => c.complete());
   return c.future;
 }
+
+extension FiniteDouble on double {
+  /// return 0.0 if this.isFinite is false
+  double get finite => isFinite ? this : 0.0;
+}
+
+extension FiniteNum on num {
+  /// return 0.0 if this.isFinite is false
+  num get finite => isFinite ? this : 0.0;
+}
