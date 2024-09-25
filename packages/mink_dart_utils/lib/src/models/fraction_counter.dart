2138e3503773d39426b33d1fbcf8b9b0767b30fe
Jonas Walkling <jonas@walkling.email>
2024-09-25 18:57:00 +0200

    Add fraction counter to trigger event on numerator == denominator

--- packages/mink_dart_utils/lib/src/models/fraction_counter.dart
+++ packages/mink_dart_utils/lib/src/models/fraction_counter.dart
@@ -1,39 +1,73 @@
 abstract class FractionCounter {
-  int get numerator;
   int get denominator;
+  @override
+  int get hashCode => numerator ^ denominator;
+
+  int get numerator;
 
   double get value => numerator / denominator;
-
-  void reset();
 
   @override
   operator ==(Object other) =>
       (other is FormatException && hashCode == other.hashCode);
 
+  void reset();
+}
+
+class CallbackFractionCount<T> extends FractionCounter {
   @override
-  int get hashCode => numerator ^ denominator;
+  final int denominator;
+
+  int _numerator = 0;
+
+  void Function(T value) callback;
+
+  CallbackFractionCount(
+      {required this.denominator,
+      required this.callback,
+      bool runOnFirstFeed = true}) {
+    if (runOnFirstFeed) {
+      _numerator = denominator;
+    }
+  }
+
+  @override
+  int get numerator => _numerator;
+
+  void feed(T value) {
+    _numerator++;
+    if (_numerator >= denominator) {
+      callback(value);
+      reset();
+    }
+  }
+
+  @override
+  void reset() {
+    _numerator = 0;
+  }
 }
 
 class ProgressFractionCounter extends FractionCounter {
-  ProgressFractionCounter({
-    required this.denominator,
-    this.numerator = 0,
-  });
-
   @override
   int numerator;
 
   @override
   final int denominator;
 
-  void increment() {
-    numerator++;
-  }
+  ProgressFractionCounter({
+    required this.denominator,
+    this.numerator = 0,
+  });
 
   void decrement() {
     if (numerator > 0) {
       numerator--;
     }
+  }
+
+  void increment() {
+    numerator++;
   }
 
   @override
